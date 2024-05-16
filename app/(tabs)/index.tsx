5c89f586752f9e90ae4e82f261835b8529460db1
Fantino-Russo <fantinojavierrusso@gmail.com>
2024-05-16 15:52:41 -0300

    Agregando componentes extra inutiles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,13 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-
+import { Puntaje } from '@/src/components/puntaje';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      
       <Text style={styles.title}>Tab One</Text>
+      <Puntaje puntos={100}></Puntaje>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
