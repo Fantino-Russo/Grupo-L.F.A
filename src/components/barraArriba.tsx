03f1516c23837d311008357e0d69ad7d301376ae
Fantino-Russo <fantinojavierrusso@gmail.com>
2024-07-30 16:44:41 -0300

    Centrando texto del header y agregando boon sidebar

--- src/components/barraArriba.tsx
+++ src/components/barraArriba.tsx
@@ -1,28 +1,40 @@
 
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Image, TouchableOpacity} from 'react-native';
 import {Text, View} from '@/components/Themed';
 import Constants from 'expo-constants';
 
 export default function BarraArriba(){
     return(
         <View style={headerStyle.Header}>
+            <TouchableOpacity>
+                <Image source={require('@/assets/images/Sidebar.png')} style={headerStyle.Imagen}/>
+            </TouchableOpacity>
+            
             <Text style={headerStyle.Titulo}>Presente!</Text>
+            <View/>
         </View>
     );
-}
+}   
 
 const headerStyle = StyleSheet.create({
     Header: {
         padding: 20,
         backgroundColor: "#2C4B9A",
-        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-between"
+        
     },
     Titulo:{
         color : "#FFF",
         fontSize: 20,
         fontWeight: "bold",
         fontFamily: "Sans-Serif",
-        
+        left: "-50%"
+
+    },
+    Imagen:{
+        width: 30,
+        height: 30,
     }
 }
 );
